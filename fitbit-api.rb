e44a6cb7a2cd8d862d158712ddb09b94925bd498
schepens83 <schepens@gmail.com>
2018-01-08 11:52:10 +0100

    diverse

---
+++ fitbit-api.rb
@@ -0,0 +1,43 @@
+require 'fitbit_api'
+require 'json'
+
+
+# read refresh token
+refresh_tkn = ""
+File.open('secret/refresh_token', 'r') do |f1|
+  refresh_tkn = f1.gets.sub(/\n$/, "")
+end
+
+# make a client
+client = FitbitAPI::Client.new(client_id: '22CHR2',
+                               client_secret: '141de0f8d38c79deed5493191c42d633',
+                               refresh_token: refresh_tkn, unit_system: 'any')
+
+
+# p client.daily_activity_summary()
+
+# result = JSON.pretty_generate(
+#   client.daily_activity_summary(date = Date.today - 1, resource: "calories", detail_level: "1min")
+#   )
+
+result = client.activity_intraday_time_series(resource = "calories", detail_level: "1min")
+
+# , start_time: "00:00:00", end_time: "00:50:00"
+
+# result = client.activity_time_series(resource = "calories", start_date: Date.today - 10)
+
+
+
+
+# temp: write result away
+File.open('result', 'w') do |f2|
+  # use "\n" for two lines of text
+  f2.puts JSON.pretty_generate(result)
+end
+
+
+# write refresh token away
+File.open('secret/refresh_token', 'w') do |f2|
+  # use "\n" for two lines of text
+  f2.puts client.refresh_token.refresh_token
+end
--- script.R
+++ script.R
@@ -1,5 +1,16 @@
-# global variables
-FITBIT_KEY    <- ">"
-FITBIT_SECRET <- ""
-Default: "http://localhost:1410/"
-FITBIT_CALLBACK <- "<your-fitbit-callback>" 
+require(tidyverse) || install.packages("tidyverse") 
+require(devtools) || install.packages("devtools")
+require(fitbitr) || devtools::install_github("teramonagi/fitbitr")
+
+
+# set some global variables
+cred <- read.csv("credentials/cred.txt")
+trim.leading <- function (x)  sub("^\\s+", "", x)
+FITBIT_KEY    <- cred %>% filter(var == "key") %>% select("val")
+FITBIT_KEY <- trim.leading(sprintf(as.character(FITBIT_KEY[,1])))
+FITBIT_SECRET <- cred %>% filter(var == "secret") %>% select("val")
+FITBIT_SECRET <- trim.leading(sprintf(as.character(FITBIT_SECRET[,1])))
+rm(cred)
+FITBIT_CALLBACK <- "http://localhost:1410" 
+
+token <- fitbitr::oauth_token()
