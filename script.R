60d54bfa9d6f7eb154a17c417b1cad22eff2476b
schepens83 <schepens@gmail.com>
2018-01-12 13:51:54 +0100

    edit R script to load from csv files

--- script.R
+++ script.R
@@ -2,15 +2,14 @@ require(tidyverse) || install.packages("tidyverse")
 require(devtools) || install.packages("devtools")
 require(fitbitr) || devtools::install_github("teramonagi/fitbitr")
 
+# DATA IMPORT -------------------------------------------------------------
+intraday_steps <- readr::read_csv("csv/activities-steps-intraday.csv")
+intraday_calories <- readr::read_csv("csv/activities-calories-intraday.csv")
 
-# set some global variables
-cred <- read.csv("credentials/cred.txt")
-trim.leading <- function (x)  sub("^\\s+", "", x)
-FITBIT_KEY    <- cred %>% filter(var == "key") %>% select("val")
-FITBIT_KEY <- trim.leading(sprintf(as.character(FITBIT_KEY[,1])))
-FITBIT_SECRET <- cred %>% filter(var == "secret") %>% select("val")
-FITBIT_SECRET <- trim.leading(sprintf(as.character(FITBIT_SECRET[,1])))
-rm(cred)
-FITBIT_CALLBACK <- "http://localhost:1410" 
 
-token <- fitbitr::oauth_token()
+# GRAPHS ------------------------------------------------------------------
+ggplot(intraday_steps) +
+  geom_line(aes(time, value))
+
+ggplot(intraday_calories) +
+  geom_line(aes(time, value))
