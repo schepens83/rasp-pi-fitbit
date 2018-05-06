72bac15103919793bab355f2be84f8912d7847eb
schepens83 <schepens@gmail.com>
2018-05-06 21:48:32 +0200

    add png to 3day sleep chart

--- script.R
+++ script.R
@@ -41,7 +41,7 @@ ggsave("charts/steps-intraday.png", device = "png", width = 155 * chart_magnifie
 
 intraday %>%
   filter(as.Date(datetime) > today() - days(3)) %>%
-  mutate(Date = as.character(as.Date(datetime))) %>% 
+  mutate(Date = as.character(as.Date(datetime))) %>%
   ggplot() +
   # geom_area(aes(update(datetime, year = 2020, month = 1, day = 1), cum_calories, alpha = Date), fill = calory_color, color = "black", position = "dodge") +
   # geom_line(aes(update(datetime, year = 2020, month = 1, day = 1), calories), color = "black", position = "dodge", size = 0.3) +
@@ -51,7 +51,7 @@ intraday %>%
   scale_x_datetime(breaks=date_breaks("6 hour"), labels=date_format("%H:%M")) +
   theme_few() +
   theme(legend.position = "bottom") +
-  labs(title = ("Calories Spent Last 3 Days"), x = "Time (hrs)", y = "Calories") 
+  labs(title = ("Calories Spent Last 3 Days"), x = "Time (hrs)", y = "Calories")
 ggsave("charts/cal-intraday.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
 
 # stat_summary(fun.y = "sum", aes(datetime, calories)) +
@@ -60,30 +60,30 @@ ggsave("charts/cal-intraday.png", device = "png", width = 155 * chart_magnifier,
 # CHARTS DAILY  ------------------------------------------------------
 # mutli month calories
 
-daily %>% 
+daily %>%
   filter(date != today) %>%
-  ggplot(aes(date, calories)) + 
-  geom_point(aes(color = calories, shape = workday), size = 2) + 
-  geom_line(alpha = 1/3) + 
+  ggplot(aes(date, calories)) +
+  geom_point(aes(color = calories, shape = workday), size = 2) +
+  geom_line(alpha = 1/3) +
   theme(legend.position = "bottom") +
   # scale_color_continuous("BrBG") +
   # scale_color_gradient_tableau("BrBG") +
-  scale_color_gradient(low="brown", high="Green", guide = "none") + 
-  geom_smooth(se = FALSE, method = "loess") + 
+  scale_color_gradient(low="brown", high="Green", guide = "none") +
+  geom_smooth(se = FALSE, method = "loess") +
   theme_few() +
   theme(legend.position = "bottom") +
   labs(title = "Calories per Day", x = "Date", y = "Calories")
 ggsave("charts/cal-date.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
 
 
-daily %>% 
+daily %>%
   filter(date != today) %>%
   ggplot(aes(x = reorder(format(date, "%b"), date), calories)) +
   geom_violin(alpha = 1/4, fill = calory_color) +
   geom_jitter(aes(color = workday, shape = vacation), alpha = 2/4) +
   labs(title = "Calories spent per day", x = "Time", y = "Calories") +
   scale_color_calc() +
-  theme_few() 
+  theme_few()
 ggsave("charts/cal-day2.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
 
 
@@ -96,37 +96,37 @@ sleep_summaries %>%
   filter(dateOfSleep > "2017-12-01") %>%
   ggplot(aes(dateOfSleep, hoursAsleep)) +
   geom_col(aes(fill = hoursAsleep)) +
-  geom_line(aes(y = hoursAwake, color = hoursAwake), size = 1) + 
+  geom_line(aes(y = hoursAwake, color = hoursAwake), size = 1) +
   scale_fill_continuous_tableau(c("Blue")) +
   scale_color_continuous_tableau("Red") +
   geom_smooth(se = FALSE, method = "loess", color = trend_color, size = 0.5, alpha = 0.2) +
-  theme_few() + 
-  theme(legend.position = "bottom") + 
+  theme_few() +
+  theme(legend.position = "bottom") +
   labs(title = "Sleep Trends", x = "Time", y = "Hours", color = "Hours Awake", fill = "Hours Asleep")
 ggsave("charts/sleep-multiday.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
 
 
-sleep_summaries %>% 
+sleep_summaries %>%
   filter(type == "stages") %>%
   filter(hoursAsleep > 5 ) %>%
   ggplot(aes(dateOfSleep, hoursAwake)) +
   geom_col(aes(fill = hoursAwake)) +
   scale_fill_continuous_tableau(c("Red")) +
   geom_smooth(se = FALSE, method = "loess", color = trend_color, size = 1) +
-  theme_few() + 
+  theme_few() +
   theme(legend.position = "bottom") +
   labs(title = "Hours awake during the night", x = "time", y = "Hours Awake", color = "Hours Awake")
 ggsave("charts/sleep-awake-multiday.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
 
-sleep_summaries %>% 
+sleep_summaries %>%
   filter(type == "stages") %>%
   filter(hoursAsleep > 5 ) %>%
   ggplot(aes(dateOfSleep, perc_awake)) +
   geom_point(aes(color = perc_awake), size = 2.5, alpha = 0.8) +
-  geom_line(aes(color = perc_awake), alpha = 1/4, size = 2.5) + 
+  geom_line(aes(color = perc_awake), alpha = 1/4, size = 2.5) +
   scale_color_continuous_tableau("Red") +
   geom_smooth(se = FALSE, method = "loess", color = trend_color) +
-  theme_few() + 
+  theme_few() +
   theme(legend.position = "bottom") +
   labs(title = "Percentage awake", x = "time", y = "% Awake", color = "% Awake")
 ggsave("charts/sleep-perc-awake-multiday.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
@@ -142,12 +142,12 @@ sleep_detailed %>%
   ggplot(aes(fix_start, level)) +
   geom_segment(aes(xend = fix_end, yend = level, color = level), size = 7) +
   facet_grid(fct_rev(as_factor(reorder(format(sleepdate, "%A"), sleepdate))) ~ .) +
-  theme_few() + 
+  theme_few() +
   scale_color_brewer(palette = "RdYlBu", direction = -1) +
   scale_x_datetime(breaks=date_breaks("2 hour"), labels=date_format("%H:%M")) +
   labs(title = "Sleep Last 3 Nights", x = "Time", y = "")
-ggsave("charts/sleep-3nights", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
-# 
+ggsave("charts/sleep-3nights.png", device = "png", width = 155 * chart_magnifier, height = 93 * chart_magnifier, units = "mm")
+#
 
 
 
